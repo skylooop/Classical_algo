88b8778729a3d4e79ba60adfc2fdc47bdbba48fe
skylooop <derivas121@gmail.com>
2019-08-20 11:59:30 +0300

    Create next_bigger_same_dgts.py

---
+++ random_katas/next_bigger_same_dgts.py
@@ -0,0 +1,7 @@
+'''Using itertools. Execution Timed Out with big n'''
+import itertools
+def next_bigger(n):
+    for num in sorted([int(''.join(map(str,x))) for x in list(set(itertools.permutations([int(x) for x in str(n)])))]):
+        if num>n:
+            return num
+    return -1
