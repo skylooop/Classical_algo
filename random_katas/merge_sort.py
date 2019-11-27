983f1bbc583fedbcb652f1cfae3ad3647c03645e
Maximilian <derivas121@gmail.com>
2019-11-27 20:09:57 +0300

    Create merge_sort.py

---
+++ random_katas/merge_sort.py
@@ -0,0 +1,29 @@
+import utils
+
+def merge(left, right):
+	A = []
+        i ,j = 0, 0
+	while i < len(left) and j < len(right):
+		if left[i] <= right[j]:
+			A.append(left[i])
+			i += 1
+		else:
+			A.append(right[j])
+			j += 1
+	A += left[i:]
+	A += right[j:]
+	return A
+
+
+def mergesort(A):
+	if len(A) > 1:
+		q = len(A) // 2
+		left = mergesort(A[:q])
+		right = mergesort(A[q:])
+		return merge(left, right)
+	return A
+
+if __name__ == "__main__":
+	A = utils.get_array()
+	print "unsorted(A): " + str(A)
+	print "mergesort(A): " + str(mergesort(A))
