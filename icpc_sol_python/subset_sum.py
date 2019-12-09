4dbef50b139706f7662cff95054c3419b131e7c8
Maximilian <derivas121@gmail.com>
2019-12-10 01:47:45 +0300

    Update subset_sum.py

--- icpc_sol_python/subset_sum.py
+++ icpc_sol_python/subset_sum.py
@@ -1,1 +1,11 @@
+#Given a set of n numbers ai sum up to M, and any K ≤ M, whether there is a subset of the
+#numbers such that they sum up to (hit) K?
 
+def subset(a,n,k):
+  if k ==0:
+    return True
+  if n==0 and k!=0:
+    return False
+  if a[n-1]>k:
+    return subset(a,n-1,k)
+  return subset(a,n-1,k) or subset(a,n-1,k-a[n-1])
