18a52d8b0874ca9c78a7b2b5839cc120f115fecb
Maximilian <derivas121@gmail.com>
2019-12-13 21:12:10 +0300

    Update longest_substring.py

--- leetcode_sol/longest_substring.py
+++ leetcode_sol/longest_substring.py
@@ -1,5 +1,7 @@
 #O(n) solution and Space O(1)
-
+#Input: "abcabcbb"
+#Output: 3 
+#Explanation: The answer is "abc", with the length of 3. 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         data = {}
