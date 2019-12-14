2feff750389438922085488d7c9d15f954916b9d
Maximilian <derivas121@gmail.com>
2019-12-14 11:39:43 +0300

    Update max_sum_subarray_better.py

--- random_katas/max_sum_subarray_better.py
+++ random_katas/max_sum_subarray_better.py
@@ -1,8 +1,11 @@
 #O(N) solution to maximum subarray sum
-def max_subar(a):
-  whole_max = a[0]
-  curr_max = a[0]
-  for i in range(len(a)):
-    curr_max = max(curr_max+a[i],curr_max)
-    whole_max = max(whole_max,curr_max)
-  return whole_max
+def maxSubArray(self, nums: List[int]) -> int:
+     maxi = float('-inf')
+     end_here = 0
+     for num in nums:
+        if end_here>0:
+            end_here +=num
+        else:
+            end_here = num
+     maxi = max(maxi,end_here)
+     return maxi
