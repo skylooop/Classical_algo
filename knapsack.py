a12a68dac0e732aca2b96e4529b3607b75a2514c
Maximilian <derivas121@gmail.com>
2020-04-23 14:58:19 +0300

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,7 +1,6 @@
 def knapSack(W, wt, val, n): 
     K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
-  
-    # Build table K[][] in bottom up manner 
+
     for i in range(n + 1): 
         for w in range(W + 1): 
             if i == 0 or w == 0: 
@@ -12,8 +11,7 @@ def knapSack(W, wt, val, n):
                 K[i][w] = K[i-1][w] 
   
     return K[n][W] 
-  
-# Driver program to test above function 
+
 val = [60, 100, 120] 
 wt = [10, 20, 30] 
 W = 50
